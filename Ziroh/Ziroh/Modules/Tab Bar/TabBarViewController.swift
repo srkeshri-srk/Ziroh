5c2f9c1ff8bf9017cc168468580cff0c5654fc29
Shreyansh <Srkeshri.srk@gmail.com>
2023-12-02 17:00:33 +0530

    Create Project base.

--- Ziroh/Ziroh/Modules/Tab Bar/TabBarViewController.swift
+++ Ziroh/Ziroh/Modules/Tab Bar/TabBarViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TabBarViewController.swift
 //  Ziroh
 //
 //  Created by Shreyansh Raj  Keshri on 02/12/23.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
